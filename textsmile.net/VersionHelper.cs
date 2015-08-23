16f1aef29166419b3d50205d2315d16cdcc906df
baguwka <ttehbb@gmail.com>
2015-08-24 00:05:57 +0300

    Bump version.

--- textsmile.net/VersionHelper.cs
+++ textsmile.net/VersionHelper.cs
@@ -3,7 +3,7 @@ using System;
 namespace textsmile.net {
    public static class VersionHelper {
       static VersionHelper() {
-         Version = new Version(0, 2);
+         Version = new Version(0, 3);
       }
 
       public static Version Version { get; private set; }
